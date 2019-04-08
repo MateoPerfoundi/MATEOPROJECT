84a8fa27a1b8e7502f8e31ba2389e1e4545b465d
MateoPerfoundi <49400181+MateoPerfoundi@users.noreply.github.com>
2019-04-08 15:25:48 +0300

    Add files via upload

---
+++ MATEO_PROJECT.ino
@@ -0,0 +1,70 @@
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__ESP8266_HARDSERIAL_POINT
+
+#include <RemoteXY.h>
+
+// RemoteXY connection settings 
+#define REMOTEXY_SERIAL Serial
+#define REMOTEXY_SERIAL_SPEED 115200
+#define REMOTEXY_WIFI_SSID "MATEO PROJECT"
+#define REMOTEXY_WIFI_PASSWORD ""
+#define REMOTEXY_SERVER_PORT 6377
+
+
+// RemoteXY configurate  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =
+  { 255,3,0,0,0,54,0,8,13,0,
+  2,0,36,9,28,26,2,26,134,31,
+  79,70,70,0,79,78,0,2,0,7,
+  9,29,26,2,26,94,97,79,70,70,
+  0,79,78,0,2,0,64,9,25,26,
+  2,26,24,31,79,70,70,0,79,78,
+  0 };
+  
+// this structure defines all the variables of your control interface 
+struct {
+
+    // input variable
+  uint8_t switch_1; // =1 if switch ON and =0 if OFF 
+  uint8_t switch_2; // =1 if switch ON and =0 if OFF 
+  uint8_t switch_3; // =1 if switch ON and =0 if OFF 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+#define PIN_SWITCH_1 12
+#define PIN_SWITCH_2 8
+#define PIN_SWITCH_3 13
+
+
+void setup() 
+{
+  RemoteXY_Init (); 
+  
+  pinMode (PIN_SWITCH_1, OUTPUT);
+  pinMode (PIN_SWITCH_2, OUTPUT);
+  pinMode (PIN_SWITCH_3, OUTPUT);
+  
+  // TODO you setup code
+  
+}
+
+void loop() 
+{ 
+  RemoteXY_Handler ();
+  
+  digitalWrite(PIN_SWITCH_1, (RemoteXY.switch_1==0)?LOW:HIGH);
+  digitalWrite(PIN_SWITCH_2, (RemoteXY.switch_2==0)?LOW:HIGH);
+  digitalWrite(PIN_SWITCH_3, (RemoteXY.switch_3==0)?LOW:HIGH);
+  
+ 
+
+}
